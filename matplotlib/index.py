2a9f016dce79f67cb555e7b96058da2a67316cdf
Mr.Steven <vanhungbkcbg1@gmail.com>
2018-02-28 10:11:08 +0700

    change import

--- matplotlib/index.py
+++ matplotlib/index.py
@@ -1,6 +1,7 @@
 from matplotlib import  pyplot as plt
-plt.plot([1,2,3],[4,5,1])
+plt.plot([1,2,3],[4,5,1],color='green',linestyle='dashed',marker='o',alpha=0.5)
 plt.title('hello')
-plt.xlabel('Ox')
+plt.xlabel('X')
 plt.ylabel('Y')
+plt.axis([0,6,0,10])
 plt.show()
